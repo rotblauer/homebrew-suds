5cb95c9bb2daeef0d64609e9bffe7b5ea294fcd2
ia <isaac.ardis@gmail.com>
2017-11-02 11:49:16 -0500

    rename formula back to suds and change stuff

--- suds.rb
+++ suds.rb
@@ -1,19 +1,18 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+require 'formula'
 
 class Suds < Formula
   desc "just testing out homebrew tap making"
-  homepage "https://rotblauer.com"
-  url "https://github.com/rotblauer/suds/releases/download/v0.0.1/suds.tar.gz"
-  sha256 "e434ef53eb83b0a3e9c93f0314dc5714b2373513be7c3e5f9f60a39fa094ee84"
+  homepage "http://rotblauer.com"
+  # url "https://github.com/rotblauer/suds/releases/download/v0.0.1/suds.tar.gz"
+  # sha256 "e434ef53eb83b0a3e9c93f0314dc5714b2373513be7c3e5f9f60a39fa094ee84"
+  url "https://github.com/rotblauer/suds.git", :tag => "v0.0.1"
 
-  # depends_on "cmake" => :build
   depends_on "go" => :build
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     ENV["GOPATH"] = buildpath
+    system "go", "env" # Debug env
     mkdir_p buildpath/"src/github.com/rotblauer/"
     ln_sf buildpath, buildpath/"src/github.com/rotblauer/suds"
     system "go", "build", "-o", "suds", "."
@@ -21,6 +20,7 @@ class Suds < Formula
   end
 
   test do
+    system "#{HOMEBREW_PREFIX}/bin/suds"
     # `test do` will create, run in and delete a temporary directory.
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
@@ -30,6 +30,5 @@ class Suds < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "true"
   end
 end
